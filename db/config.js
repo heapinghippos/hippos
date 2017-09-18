38e0a9958410e916374e1488c1ce88651e10adc9
gitBizzy10 <butvin@gmail.com>
2017-09-18 19:11:10 -0400

    Connect Mongo database, create citySchema, create userSchema

--- db/config.js
+++ db/config.js
@@ -2,12 +2,9 @@ const mongoose = require('mongoose');
 
 const db = mongoose.connection;
 
-db.on('error', function() {
-	console.error('EARroar!');
-});
+db.on('error', console.error('EARroar!'));
 db.once('open', function() {
   console.log('The doors are open! The doors are open.')
-
 })
 
 mongoose.connect('mongodb://hippo:hippo@ds141524.mlab.com:41524/destinations_hippo')
