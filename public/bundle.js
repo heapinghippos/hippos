e4d432586fb7fe5d7cb8e75cef28fae82f302780
Max Peng <pengcheng95@gmail.com>
2017-09-18 19:33:02 -0400

    created components for display as well as fake data

--- public/bundle.js
+++ public/bundle.js
@@ -22400,7 +22400,7 @@ module.exports = __webpack_require__(23);
 
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -22409,9 +22409,13 @@ var _react = __webpack_require__(183);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactDom = __webpack_require__(82);
+var _NavBar = __webpack_require__(185);
 
-var _reactDom2 = _interopRequireDefault(_reactDom);
+var _NavBar2 = _interopRequireDefault(_NavBar);
+
+var _Display = __webpack_require__(186);
+
+var _Display2 = _interopRequireDefault(_Display);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -22422,32 +22426,96 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var App = function (_React$Component) {
-	_inherits(App, _React$Component);
+  _inherits(App, _React$Component);
 
-	function App(props) {
-		_classCallCheck(this, App);
+  function App(props) {
+    _classCallCheck(this, App);
 
-		var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
 
-		_this.state = {};
-		return _this;
-	}
+    _this.state = {
+      currentCities: [{
+        name: 'London',
+        dateOfArrival: '2017-06-25',
+        dateofDeparture: '2017-07-02'
+      }, {
+        name: 'Paris',
+        dateOfArrival: '2017-07-25',
+        dateofDeparture: '2017-08-02'
 
-	_createClass(App, [{
-		key: 'render',
-		value: function render() {
-			return _react2.default.createElement(
-				'div',
-				null,
-				'app is rendering'
-			);
-		}
-	}]);
+      }],
+      tags: []
 
-	return App;
+    };
+    return _this;
+  }
+
+  _createClass(App, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        null,
+        _react2.default.createElement(_NavBar2.default, null),
+        _react2.default.createElement(_Display2.default, null)
+      );
+    }
+  }]);
+
+  return App;
 }(_react2.default.Component);
 
 exports.default = App;
+
+/***/ }),
+/* 185 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _react = __webpack_require__(183);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function NavBar(props) {
+	return _react2.default.createElement('div', null);
+}
+
+exports.default = NavBar;
+
+/***/ }),
+/* 186 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _react = __webpack_require__(183);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Display(props) {
+	return _react2.default.createElement(
+		'div',
+		null,
+		' '
+	);
+}
+
+exports.default = Display;
 
 /***/ })
 /******/ ]);
