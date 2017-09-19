35ae76aec466293e3c7d3e7abf34ae042381a8ae
Eric Powell <EricPowell85@gmail.com>
2017-09-19 10:39:02 -0400

    redit the app.jsx

--- public/App.jsx
+++ public/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import InputBar from './InputBar.jsx';
-=======
-import NavBar from './NavBar.jsx';
->>>>>>> created components for display as well as fake data
+import InputBar from './InputBar.jsx';
 import Display from './Display.jsx'
 
 class App extends React.Component {
@@ -11,24 +8,8 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-<<<<<<< HEAD
       currentCities: [],
       tags: ''
-=======
-      currentCities: [{
-        name: 'London',
-        dateOfArrival: '2017-06-25',
-        dateofDeparture: '2017-07-02'
-      },
-      {
-        name: 'Paris',
-        dateOfArrival: '2017-07-25',
-        dateofDeparture: '2017-08-02'
-
-      }],
-      tags: []
->>>>>>> created components for display as well as fake data
-
 		}
     this.addCity = this.addCity.bind(this);
     this.addTags = this.addTags.bind(this);
@@ -58,11 +39,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-<<<<<<< HEAD
         <InputBar addCityToParent={this.addCity} addTagsToParent={this.addTags}/>
-=======
-        <NavBar/>
->>>>>>> created components for display as well as fake data
         <Display/>
 			</div>
 		)
