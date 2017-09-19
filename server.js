c9371c50af9c3303e16b6f74b5f92fb098b3d27a
Max Peng <pengcheng95@gmail.com>
2017-09-19 15:44:48 -0400

    ui routing created

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ var app = express();
 var port = 3000;
 app.use(express.static('./public'));
 
+app.get('/', function(req, res) {
+	res.redirect('/#/');
+})
+
+app.get('/login', function(req, res) {
+	console.log('testing');
+	res.redirect('/#/login');
+})
+
 http.createServer(app).listen(port);
 console.log(`Listening on ${port}...`)
 
