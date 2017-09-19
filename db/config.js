29f909cb7f1af42b4e0b73a90c464a832405bb21
minwoo_kim <kminwoo0710@gmail.com>
2017-09-19 17:08:00 -0400

    it is a re-base model for everyone

--- db/config.js
+++ db/config.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const db = mongoose.connection;
-
-
-db.on('error', console.error('EARroar!'));
-db.once('open', function() {
-  console.log('The doors are open! The doors are open.')
-}
+// const db = mongoose.connection;
 
 
-mongoose.connect('mongodb://hippo:hippo@ds141524.mlab.com:41524/destinations_hippo')
+// db.on('error', console.error('EARroar!'));
+// db.once('open', function() {
+//   console.log('The doors are open! The doors are open.')
+// })
 
-module.exports = db
+
+// mongoose.connect('mongodb://hippo:hippo@ds141524.mlab.com:41524/destinations_hippo')
+
+// module.exports = db
